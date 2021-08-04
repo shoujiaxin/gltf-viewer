0aaf91bb412651b6b2397e3110ef727b89039ebc
Jiaxin Shou <shoujiaxin@gmail.com>
2021-08-04 21:49:03 +0800

    feat: initial commit

---
+++ glTFViewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  glTFViewer
+//
+//  Created by Jiaxin Shou on 2021/8/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ glTFViewer/glTFViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  glTFViewerApp.swift
+//  glTFViewer
+//
+//  Created by Jiaxin Shou on 2021/8/4.
+//
+
+import SwiftUI
+
+@main
+struct glTFViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
