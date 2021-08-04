eca25e10cd1e4deedae188a9681253139959d4d6
Jiaxin Shou <shoujiaxin@gmail.com>
2021-08-05 01:50:18 +0800

    feat: add preview scene

--- glTFViewer/Views/ModelView.swift
+++ glTFViewer/Views/ModelView.swift
@@ -27,6 +27,7 @@ struct ModelView: View {
 
 struct ModelView_Previews: PreviewProvider {
     static var previews: some View {
-        ModelView(url: nil)
+        ModelView(url: URL(fileURLWithPath: "Preview Scene.scnassets/PrimaryIonDrive.glb", relativeTo: Bundle.main.resourceURL))
+            .frame(width: 500, height: 500)
     }
 }
