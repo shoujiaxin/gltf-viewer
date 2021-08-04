2773c560323eb0353a72e4f7ddaab6502c27ddfb
Jiaxin Shou <shoujiaxin@gmail.com>
2021-08-04 21:55:36 +0800

    feat: update window appearance

--- glTFViewer/glTFViewerApp.swift
+++ glTFViewer/glTFViewerApp.swift
@@ -12,6 +12,17 @@ struct glTFViewerApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .frame(minWidth: minWindowWidth, minHeight: minWindowHeight)
+                .toolbar {
+                    Button(action: {}) {
+                        Image(systemName: "gearshape")
+                    }
+                }
         }
     }
+
+    // MARK: - Constants
+
+    private let minWindowWidth: CGFloat = 480
+    private let minWindowHeight: CGFloat = 360
 }
