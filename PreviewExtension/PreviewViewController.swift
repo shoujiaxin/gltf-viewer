1ffea069f1a83e1b446e2aa9de0a5be10977f9af
Jiaxin Shou <shoujiaxin@gmail.com>
2021-08-06 00:44:45 +0800

    feat: add quicklook preview extension

---
+++ PreviewExtension/PreviewViewController.swift
@@ -0,0 +1,41 @@
+//
+//  PreviewViewController.swift
+//  PreviewExtension
+//
+//  Created by Jiaxin Shou on 2021/8/6.
+//
+
+import Cocoa
+import GLTFSceneKit
+import Quartz
+import SceneKit
+
+class PreviewViewController: NSViewController, QLPreviewingController {
+    @IBOutlet var sceneView: SCNView!
+
+    override var nibName: NSNib.Name? {
+        return NSNib.Name("PreviewViewController")
+    }
+
+    override func loadView() {
+        super.loadView()
+
+        sceneView.allowsCameraControl = false
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.backgroundColor = .clear
+    }
+
+    func preparePreviewOfFile(at url: URL, completionHandler handler: @escaping (Error?) -> Void) {
+        let scene: SCNScene
+        do {
+            let source = GLTFSceneSource(url: url)
+            scene = try source.scene()
+        } catch {
+            handler(error)
+            return
+        }
+
+        sceneView.scene = scene
+        handler(nil)
+    }
+}
